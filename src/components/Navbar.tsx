119282898339ceb1a232ead0c93f1518605f142b
Zabdiel Perez <zabdielperez@Zabdiels-MacBook-Air.local>
2024-12-13 13:54:38 -0800

    Added logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai' 
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
     // Grab user name
@@ -17,7 +18,8 @@ const Navbar = () => {
   return (
     <>
     <div className="flex justify-center items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-        <h1 className="w-full text-3xl font-bold text-[#38c292]">Aura</h1>
+        <img src={logo} alt="Aura Logo" className="w-[420px] h-[420px] object-contain pr-11"/>
+        {/* <h1 className="w-full text-3xl font-bold text-[#38c292]">Aura</h1> */}
         <ul className="hidden md:flex">
             <li className="p-4"><a href='/'>Home</a></li>
             <li className="p-4">Generate</li>
