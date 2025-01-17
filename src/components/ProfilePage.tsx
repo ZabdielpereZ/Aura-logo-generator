d850dbd820e02e93833c616b35eb7e17bdff8bf1
Zabdiel Perez <zabdielperez@Zabdiels-MacBook-Air.local>
2025-01-17 12:28:12 -0800

    Handling delete in profile page

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -16,8 +16,8 @@ const ProfilePage = () => {
   }, []);
 
   // removes the image from both the state and local storage.The images are displayed with a corresponding delete button for each one.
-  const handleDelete = (image: string) => {
-    const updatedImages = images.filter((img) => img !== image);
+  const handleDelete = (index: number) => {
+    const updatedImages = images.filter((_, idx) => index !== idx);
     setImages(updatedImages);
     localStorage.setItem("images", JSON.stringify(updatedImages));
   };
@@ -53,9 +53,9 @@ const ProfilePage = () => {
           {/* deleted images by index */}
           <div className="md:container dark:bg-gray-900 flex flex-wrap justify-center pb-5 space-x-5">
             {images.length > 0 && (
-                images.map((image) => (
+                images.map((image, index) => (
                   <div className="first:ml-5 w-1/3">
-                  <button className="text-[#023] bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-[#38c292] dark:hover:bg-[#217658] dark:focus:ring-green-900" onClick={() => handleDelete(image)}>Delete</button>{" "}
+                  <button className="text-[#023] bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-[#38c292] dark:hover:bg-[#217658] dark:focus:ring-green-900" onClick={() => handleDelete(index)}>Delete</button>{" "}
                   <ImageGallery image={image} />
                   </div>
                 ))
