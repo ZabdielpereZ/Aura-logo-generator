caadef23fcc9cce98ef36fab43c1309e9bdf722e
Zabdiel Perez <zabdielperez@Zabdiels-MacBook-Air.local>
2025-01-17 15:20:38 -0800

    fixing grammar

--- src/components/DataBase.tsx
+++ src/components/DataBase.tsx
@@ -4,7 +4,7 @@ export const Leaderboard = [
     // }
 
     {
-        name: "Sally McSalad",
+        name: "Sally Mclad",
         location: "India",
         score : 1550,
         img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
@@ -12,7 +12,7 @@ export const Leaderboard = [
         image:"https://i.pinimg.com/736x/ce/4c/47/ce4c472ad780b837b8e58e20d091002a.jpg"
     },
     {
-        name: "Joe Mama" ,
+        name: "Joe Ama" ,
         location: "USA",
         score : 1310,
         img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
@@ -20,7 +20,7 @@ export const Leaderboard = [
         image:"https://i.pinimg.com/736x/49/98/85/4998854699c65e68cf3f28482adb8fc6.jpg"
     },
     {
-        name: "Terri Yaki",
+        name: "Terry Soma",
         location: "China",
         score : 350,
         img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
@@ -36,7 +36,7 @@ export const Leaderboard = [
         image:"https://i.pinimg.com/736x/55/37/cb/5537cbc9f444309661aba847eaf74ed3.jpg"
     },
     {
-        name: "Peggy Wallace",
+        name: "Jerry Wallace",
         location: "London",
         score : 1250,
         img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
