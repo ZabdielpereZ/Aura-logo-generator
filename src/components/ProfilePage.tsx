3270672e607c2aa7f08b70a7aa8c0aefacd2879d
Zabdiel Perez <zabdielperez@Zabdiels-MacBook-Air.local>
2025-01-17 12:12:50 -0800

    fixing css

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -51,10 +51,10 @@ const ProfilePage = () => {
             )}{" "}
           </div> */}
           {/* deleted images by index */}
-          <div className="md:container md:mx-auto dark:bg-gray-900 flex flex-wrap justify-center pb-5 space-x-5">
+          <div className="md:container dark:bg-gray-900 flex flex-wrap justify-center pb-5 space-x-5">
             {images.length > 0 && (
                 images.map((image) => (
-                  <div className="w-1/3">
+                  <div className="first:ml-5 w-1/3">
                   <button className="text-[#023] bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-[#38c292] dark:hover:bg-[#217658] dark:focus:ring-green-900" onClick={() => handleDelete(image)}>Delete</button>{" "}
                   <ImageGallery image={image} />
                   </div>
